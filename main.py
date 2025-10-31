1741dd30daf0fce1437e4e994e59a65f689c1fa6
susamogus@archlinux <a5457789889409@gmail.com>
2025-10-31 21:51:38 +0800

    Main.py file adapted from template

--- main.py
+++ main.py
@@ -3,19 +3,31 @@ from discord.ext import commands
 from config.constants import command_prefix, MY_GUILD
 from config.secrets import bot_token, DATABASE_URL
 import asyncio
+from db.base import Base
+from core.leetcode_problem import LeetCodeProblemManager
+from core.leetcode_api import LeetCodeAPI
 from db.database_manager import DatabaseManager
 from sqlalchemy import create_engine
+import os
 
 
-class MyBot(commands.Bot):
+class LeetCodeBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.all()
         super().__init__(command_prefix=command_prefix, intents=intents)
         self.engine = create_engine(DATABASE_URL, echo=True)
         self.database_manager = DatabaseManager(self, self.engine)
+        self.leetcode_api = LeetCodeAPI()
+        self.leetcode_problem_manger: LeetCodeProblemManager = LeetCodeProblemManager(
+            leetcode_api=self.leetcode_api,
+            database_manager=self.database_manager,
+        )
 
     async def setup_hook(self) -> None:
-        await self.load_extension("cogs.general")
+        for cog in os.listdir("cogs"):
+            if cog.endswith(".py"):
+                await self.load_extension(f"cogs.{cog[:-3]}")
+        await self.leetcode_problem_manger.init_cache()
 
     async def close(self) -> None:
         await super().close()
@@ -28,7 +40,8 @@ class MyBot(commands.Bot):
 
 
 async def main():
-    bot = MyBot()
+    bot = LeetCodeBot()
+    Base.metadata.create_all(bind=bot.engine)
     try:
         await bot.start(token=bot_token)
     except KeyboardInterrupt:
